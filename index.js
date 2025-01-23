84dc49ffdd0f42e6fb7d64312e3b8a3668abab38
shyam pathade <shyam.pathade55@gmail.com>
2025-01-23 12:27:28 +0530

    add changes to

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+
+app.use("/", express.static(__dirname + "/"));
 app.get("/", (req, res) => res.send("Congratulation 🎉🎉! Our Express server is Running on Vercel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
